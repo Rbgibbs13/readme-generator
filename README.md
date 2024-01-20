0f5572b8190f6bbdcc06bfbdf97d4df77f2d0d3f
unknown <rhett.gibbs@yahoo.com>
2024-01-19 18:14:39 -0800

    Added license descriptions - cleaned up some useless code

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Generates a Professional ReadMe through Command Line Iinterface prompts.
               | [Questions - Contact Me](#questions) |
 
 
-# Install
+# Installation
 Clone the repository to your local machine, or download the zip and extract the files to your local machine.
 
 
@@ -26,7 +26,7 @@ Access the index.js file in the package through the Command Line Interface of yo
 Garrett Gibbs 2024
 
 
-# Test
+# Tests
 To test, follow same directions as usage but use random inputs!
 
 
@@ -34,11 +34,30 @@ To test, follow same directions as usage but use random inputs!
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) MIT
 
 
+MIT License
+
+Copyright (c) 2024 Rbgibbs13
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 # Questions
-
-
-Github: https://github.com/Rbgibbs13
+Github: https://github.com/Rbgibbs13 rbgibbs13
 
 Email: rhett.gibbs@yahoo.com
 
-Best Method: Email
+Best Contact Method: Email
--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 const inquirer = require("./packages/node_modules/inquirer");
 const fs = require("fs");
-//import inquirer from "inquirer";
-//Title
-//Description
-//Table of Contents
-//Installation
-//Usage
-//License
-//Contributing
-//Tests
-//Questions - github username github link + email + how to reach me
 
-//License Badge - License Explanation
+//Copied from License in repository :)
+let MITdescription = `
+MIT License
+
+Copyright (c) 2024 Rbgibbs13
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`
+
+//Copied from Wikipedia
+let ISCdescription = `
+ISC License
+
+Copyright 2024 Garrett Gibbs
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`
+
+//Taken from Wikipedia https://en.wikipedia.org/wiki/Apache_License && https://www.apache.org/legal/src-headers.html#notice
+let Apachedescription = `
+The Apache License is a free software license. 
+The user is granted a patent license from each contributor to "make, have made, use, offer to sell, sell, import or otherwise transfer the work.
+
+Apache ReadMe Generator
+Copyright 2024 The Apache Software Foundation
+
+This product includes software developed at
+The Apache Software Foundation (http://www.apache.org/).`
+
+//taken from Wikipedia https://en.wikipedia.org/wiki/GNU_General_Public_License
+let GNUdescription = `
+GNU GPL v3.0
+The GNU General Public License is a collection of free software licenses that guarantee users the ability to run, study, share, or modify the software.`
+
+let BSDdescription = `
+BSD License 2.0 Generic
+Copyright 2024 Garrett Gibbs
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+   3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`;
 
 inquirer.prompt([
     {
@@ -83,7 +133,7 @@ inquirer.prompt([
     let GNULicenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
     let BSDLicenseBadge = "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
     let ISCLicenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-
+    
     let build = "";
     build += `# ${title}\n\n\n`;
     build += `# Description\n\n${description}\n\n\n`;
@@ -92,28 +142,28 @@ inquirer.prompt([
               | ${linkInstall} | | ${linkUsage} |
               | ${linkContribute} | | ${linkLicense} |
               | ${linkQuestion} |\n\n\n`
-    build += `# Install\n${installation}\n\n\n`;
+    build += `# Installation\n${installation}\n\n\n`;
     build += `# Usage\n${usage}\n\n\n`;
     build += `# Contributors\n${contributions}\n\n\n`;
-    build += `# Test\n${test}\n\n\n`;
+    build += `# Tests\n${test}\n\n\n`;
 
     if(license === "MIT") {
         console.log("MIT License");
-        build += `# License \n${MITLicenseBadge} ${license}\n\n\n`;
+        build += `# License \n${MITLicenseBadge} ${license}\n\n${MITdescription}\n`;
     } else if(license === "GNU GPL 3.0") {
-        build += `# License \n${GNULicenseBadge} ${license}\n\n\n`;
+        build += `# License \n${GNULicenseBadge} ${license}\n\n${GNUdescription}\n`;
     } else if(license === "Apache 2.0") {
-        build += `# License \n${apacheLicenseBadge} ${license}\n\n\n`;
+        build += `# License \n${apacheLicenseBadge} ${license}\n\n${Apachedescription}\n`;
     } else if(license === "BSD-2") {
-        build += `# License \n${BSDLicenseBadge} ${license}\n\n\n`;
+        build += `# License \n${BSDLicenseBadge} ${license}\n\n${BSDdescription}\n`;
     } else {
-        build += `# License \n${ISCLicenseBadge} ${license}\n\n\n`;
+        build += `# License \n${ISCLicenseBadge} ${license}\n\n${ISCdescription}\n`;
     }
     
     build += `# Questions\n`;
     build += `Github: ${username}\n\n`
     build += `Email: ${email}\n\n`;
-    build += `Best Method: ${contact}`;
+    build += `Best Contact Method: ${contact}`;
 
     fs.writeFile("README.md", build, (error) => 
         error ? console.error(error) : console.log("Saved Commit to ReadMe") 
